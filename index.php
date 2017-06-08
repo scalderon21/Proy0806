3bdfe2b6a7dff1b9b63aa210285669f86d5ed5da
cetecom <cetecom@LC08L1219>
2017-06-08 12:31:55 -0400

    Login

--- index.php
+++ index.php
@@ -7,11 +7,31 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
+        <script src="js/jquery-3.2.1.min.js" type="text/javascript"></script>
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <form id="frmusuario">
+            <div><label>Usuario:</label>><input type="text" name="nomusuario" id="nomusuario"></div>
+            <div><label>Clave:</label>><input type="password" name="clave" id="clave"></div>
+            <input id="enviar" type="button" onclick="" value="Enviar">
+            
+        </form>
     </body>
+    <script>
+    $(document).ready(function(){
+        $("#enviar").click(function(){
+            /*$("form").hide();
+            alert("Ocultarse el formulario :-)" + $("#nomusuario").val());*/
+        if($("#nomusuario").val()!="" && $("#clave").val()!=""){
+            $("frmusuario").submit();
+        }
+        else
+        {
+            alert("Debe agregar el usuario y clave");
+            
+        }
+        });
+    });
+    </script>
 </html>
